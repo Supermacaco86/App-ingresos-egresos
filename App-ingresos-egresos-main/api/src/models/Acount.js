d53defab7a0d8a7c2d56c092a4579824c08b5f97
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-09-29 17:50:15 -0300

    models

--- App-ingresos-egresos-main/api/src/models/Acount.js
+++ App-ingresos-egresos-main/api/src/models/Acount.js
@@ -3,8 +3,16 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define('acount', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_account: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name_account: {
       type: DataTypes.STRING,
       allowNull: false,
     },
---
+++ App-ingresos-egresos-main/api/src/models/Staff.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('staff', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    document: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    adress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    avatar: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+  });
+};
