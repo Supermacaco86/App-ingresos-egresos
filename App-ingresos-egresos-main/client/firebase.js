03f398620a235256fb34a78c581dfe19dd4585a3
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-10-01 16:59:55 -0300

    comienzo login firebase y store

---
+++ App-ingresos-egresos-main/client/firebase.js
@@ -0,0 +1,20 @@
+/// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBotmJsWmYUGsop3p9Tj1iBAt7oPIk27So",
+  authDomain: "api-ingresos-egresos.firebaseapp.com",
+  databaseURL: "https://api-ingresos-egresos-default-rtdb.firebaseio.com",
+  projectId: "api-ingresos-egresos",
+  storageBucket: "api-ingresos-egresos.appspot.com",
+  messagingSenderId: "835395366806",
+  appId: "1:835395366806:web:82d7852bb0a11ca113b7b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
