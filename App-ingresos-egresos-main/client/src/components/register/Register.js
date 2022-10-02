676f6b4209792e144f4ef2bf23955ae989d7ee5f
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-10-01 22:38:04 -0300

    register

--- App-ingresos-egresos-main/client/src/components/register/Register.js
+++ App-ingresos-egresos-main/client/src/components/register/Register.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 
 export default function Register(){
+
+    const[user, setUser]=useState({
+        email:'',
+        password:'',
+    })
+
+    const handleChange = e => {
+        console.log(e)
+    }
+
     return(
-        <h1>Register</h1>
+        <form>
+            <label htmlFor="email">Email</label>
+            <input 
+            type="email" 
+            name="email" 
+            placeholder="email"
+            onChange={handleChange}/>
+
+            <label htmlFor="password">Password</label>
+            <input 
+            type="passsword" 
+            name="password"
+            placeholder="password" 
+            id="password"
+            onChange={handleChange}/>
+
+            <button>Registrate</button>
+        </form>
     )
 } 
