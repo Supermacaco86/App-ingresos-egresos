7a4ed41fada49ae0e8f0048c840289e4ac9efc94
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-10-01 22:01:11 -0300

    Ruteo y componetes

--- App-ingresos-egresos-main/client/src/App.js
+++ App-ingresos-egresos-main/client/src/App.js
@@ -5,6 +5,8 @@ import {Routes} from "react-router-dom";
 import {BrowserRouter} from "react-router-dom";
 import LandingPage from "./components/landingPage/LandingPage"
 import Home from "./components/home/Home"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' exact element={<LandingPage/>}/>
           <Route path='/Home' exact element={<Home/>}/>
+          <Route path='/Login' exact element={<Login/>}/>
+          <Route path='/Register' exact element={<Register/>}/>
         </Routes>
       </div>
     </BrowserRouter>
